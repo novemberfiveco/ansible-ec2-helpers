b3c2dcba3a14dbbda0f9babd800d697c7c6256c2
Ruben Van den Bossche <rubenvdb@gmail.com>
2015-04-15 15:49:41 +0200

    Rename inventory; add install_ansible script

---
+++ scripts/ubuntu/install_ansible.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+# Script to be executed from userdata. Installs ansible, boto
+# awscli.
+
+echo "*** Install Ansible, git, boto"
+
+# install apt-get packages for git and python-pip
+sudo apt-get -y install git python-dev python-pip
+
+# install pip packages for ansible, awscli and boto
+sudo pip install ansible
+sudo pip install awscli
+sudo pip install boto
+
+echo "*** Install ansible EC2 local inventory"
+sudo mkdir -p /etc/ansible
+sudo wget https://github.com/Appstrakt/ansible-ec2-helpers/archive/develop.zip -O /tmp/ansible-ec2-helpers.zip
+sudo unzip /tmp/ansible-ec2-helpers.zip -d /tmp
+sudo mv /tmp/ansible-ec2-helpers-develop /etc/ansible/ec2
